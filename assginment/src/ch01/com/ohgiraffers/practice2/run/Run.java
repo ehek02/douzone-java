4b0b5602a71fd921a09d5b536d59f37f4a277383
ehek01 <whdgus0515@gmail.com>
2022-08-03 18:52:13 +0900

    3장 완료

---
+++ assginment/src/ch01/com/ohgiraffers/practice2/run/Run.java
@@ -0,0 +1,12 @@
+package ch01.com.ohgiraffers.practice2.run;
+
+import ch01.com.ohgiraffers.practice2.example.CastingPractice;
+
+public class Run {
+    public static void main(String[] args) {
+        CastingPractice castingPractice = new CastingPractice();
+//        castingPractice.method1();
+//        castingPractice.method2();
+        castingPractice.method3();
+    }
+}
